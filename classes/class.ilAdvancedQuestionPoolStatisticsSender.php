ddbb4c87d1b817939f55da09f21bc71371d519e9
Theodor Truffer <tt@studer-raimann.ch>
2018-11-01 16:19:24 +0100

    LAT-408 (10)

--- classes/class.ilAdvancedQuestionPoolStatisticsSender.php
+++ classes/class.ilAdvancedQuestionPoolStatisticsSender.php
@@ -10,15 +10,13 @@ class ilAdvancedQuestionPoolStatisticsSender {
 
 
 	public function createNotification($course_id, $usr_id, $ref_id, $trigger, $trigger_values){
-        global $ilCtrl;
         $sender = new srNotificationInternalMailSender(new ilObjUser(6), new ilObjUser($usr_id));
         $qst_pool = new ilObjQuestionPool($ref_id,true);
 
-        $ilCtrl->setParameterByClass('ilObjQuestionPoolGUI', 'ref_id', $trigger->getRefId());
         $placeholders = array(
             'course' => new ilObjCourse($course_id,true),
             'qst_pool' => $qst_pool,
-            'qst_pool_url' => ILIAS_HTTP_PATH . '/' . $ilCtrl->getLinkTargetByClass('ilObjQuestionPoolGUI'),
+            'qst_pool_url' => ILIAS_HTTP_PATH . '/goto.php?target=qpl_' . $ref_id . '&client_id=' . CLIENT_ID,
             'trigger' => $trigger,
             'trigger_values' => $trigger_values
         );
