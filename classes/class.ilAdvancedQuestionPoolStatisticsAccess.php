515eb00e52ae4943e4629b95f671d9a4d2155fe9
Theodor Truffer <tt@studer-raimann.ch>
2020-06-05 09:30:50 +0200

    fixed autoloader

--- classes/class.ilAdvancedQuestionPoolStatisticsAccess.php
+++ classes/class.ilAdvancedQuestionPoolStatisticsAccess.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once'./libs/composer/vendor/autoload.php';
 
-//TODO ACCESS CONTROL
 class ilAdvancedQuestionPoolStatisticsAccess {
 
 	/**
